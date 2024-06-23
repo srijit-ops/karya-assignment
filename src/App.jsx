2632096db4e36a3503fe493a16089ee9d4eb076f
srijit-ops <srijitasengupta23@gmail.com>
2024-06-23 20:48:10 +0530

    responsiveness added

--- src/App.jsx
+++ src/App.jsx
@@ -5,20 +5,20 @@ import { data } from "./utils/data";
 const moveRestrictionVertical = 25;
 const moveRestrictionHorizontal = 25;
 
-const val = {
+const defaultContent = {
   mainLogo: "/karya-logo.svg",
 };
 
 const dynamicStyles = [
-  "top-[250px] left-[250px]",
-  "top-[450px] left-[150px]",
+  "lg:top-[250px] xl:left-[250px] lg:left-[190px] sm:left-[350px] md:top-[80px] md:left-[400px] top-[90px] left-[290px]",
+  "lg:top-[450px] lg:left-[150px] md:top-[620px] md:left-[250px] top-[630px] left-[225px] sm:left-[190px]",
   "right-[110px] top-[70px]",
-  "top-[450px] right-[160px]",
-  "top-[550px] right-[300px]",
-  "top-[280px] right-[90px]",
-  "top-[190px] right-[300px]",
+  "lg:top-[450px] lg:right-[160px] md:top-[620px] md:right-[180px] top-[670px] right-[165px]",
+  "lg:top-[550px] md:top-[620px] top-[620px]  right-[300px]",
+  "lg:top-[280px] right-[30px] md:top-[530px] top-[680px] sm:top-[600px]",
+  "lg:top-[190px] xl:right-[300px] lg:right-[200px] md:top-[50px] md:right-[450px] sm:right-[370px] right-[290px] top-[50px]",
   "top-[100px] left-[100px]",
-  "top-[530px] left-[325px]",
+  "xl:top-[550px] xl:left-[290px] lg:top-[570px] lg:left-[100px] sm:top-[630px] top-[680px] left-[70px]",
 ];
 
 function App() {
@@ -27,7 +27,7 @@ function App() {
 
   const ref = useRef(null);
 
-  const [state, setState] = useState(val);
+  const [state, setState] = useState(defaultContent);
 
   const handleMouseMove = (e) => {
     if (ref.current) {
@@ -61,7 +61,6 @@ function App() {
         onMouseMove={handleMouseMove}
       >
         <svg
-          // className="testimonials_circles_"
           xmlns="http://www.w3.org/2000/svg"
           width="1600"
           height="1400"
@@ -73,10 +72,10 @@ function App() {
             cy="700"
             r="718.5"
             fill="#d7f8e8" //#d1def9
-            fill-opacity="0.2"
+            fillOpacity="0.2"
             stroke="#bfe9d3"
-            stroke-width="2"
-            stroke-dasharray="6 10"
+            strokeWidth="2"
+            strokeDasharray="6 10"
             style={{ x, y }}
           ></motion.circle>
           <motion.circle
@@ -84,10 +83,10 @@ function App() {
             cy="700"
             r="658.5"
             fill="#d7f8e8"
-            fill-opacity="0.2"
+            fillOpacity="0.2"
             stroke="#bfe9d3"
-            stroke-width="2"
-            stroke-dasharray="4 10"
+            strokeWidth="2"
+            strokeDasharray="4 10"
             style={{ x, y }}
           ></motion.circle>
           <motion.circle
@@ -95,10 +94,10 @@ function App() {
             cy="700"
             r="638.5"
             fill="#d7f8e8"
-            fill-opacity="0.2"
+            fillOpacity="0.2"
             stroke="#bfe9d3"
-            stroke-width="2"
-            stroke-dasharray="9 10"
+            strokeWidth="2"
+            strokeDasharray="9 10"
             style={{ x, y }}
           ></motion.circle>
           <motion.circle
@@ -106,10 +105,10 @@ function App() {
             cy="700"
             r="528.5"
             fill="#d7f8e8"
-            fill-opacity="0.2"
+            fillOpacity="0.2"
             stroke="#b7e7ce"
-            stroke-width="2"
-            stroke-dasharray="6 10"
+            strokeWidth="2"
+            strokeDasharray="6 10"
             style={{ x, y }}
           ></motion.circle>
           <motion.circle
@@ -117,10 +116,10 @@ function App() {
             cy="700"
             r="418.5"
             fill="#d7f8e8"
-            fill-opacity="0.2"
+            fillOpacity="0.2"
             stroke="#b4e6cb"
-            stroke-width="2"
-            stroke-dasharray="6 10"
+            strokeWidth="2"
+            strokeDasharray="4 10"
             style={{ x, y }}
           ></motion.circle>
           <motion.circle
@@ -128,21 +127,10 @@ function App() {
             cy="700"
             r="398.5"
             fill="#d7f8e8"
-            fill-opacity="0.2"
+            fillOpacity="0.2"
             stroke="#b8deca"
-            stroke-width="2"
-            stroke-dasharray="4 10"
-            style={{ x, y }}
-          ></motion.circle>
-          <motion.circle
-            cx="700"
-            cy="700"
-            r="378.5"
-            fill="#d7f8e8"
-            fill-opacity="0.2"
-            stroke="#9fdebc"
-            stroke-width="2"
-            stroke-dasharray="6 10"
+            strokeWidth="2"
+            strokeDasharray="6 10"
             style={{ x, y }}
           ></motion.circle>
           <motion.circle
@@ -150,25 +138,25 @@ function App() {
             cy="700"
             r="318.5"
             fill="#d7f8e8"
-            fill-opacity="0.2"
+            fillOpacity="0.2"
             stroke="#8cdcb2"
-            stroke-width="2"
-            stroke-dasharray="4 10"
+            strokeWidth="2"
+            strokeDasharray="4 10"
             style={{ x, y }}
           ></motion.circle>
         </svg>
-        <div className="absolute top-1/2 left-1/2 z-[500] h-[350px] w-[420px] pointer-events-none -translate-x-1/2 -translate-y-1/2 select-none">
+        <div className="absolute top-1/2 left-1/2 z-[500] h-[350px] sm:w-[420px] w-11/12 pointer-events-none -translate-x-1/2 -translate-y-1/2 select-none flex justify-center items-center flex-col">
           {state.mainLogo ? (
             <div className="flex justify-center items-center">
               <img
                 alt="karya-logo"
                 src={state.mainLogo}
-                className="w-96 h-96"
+                className="lg:w-96 lg:h-96 sm:w-72 sm:h-72 w-56 h-56"
               />
             </div>
           ) : (
             <>
-              <h1 className="font-bold text-xl tracking-wide mb-8 text-[#1e293b] text-center">
+              <h1 className="font-bold sm:text-xl text-lg tracking-wide sm:mb-8 mb-6 text-[#1e293b] text-center">
                 Trusted by the world's top companies
               </h1>
               <div className="flex justify-center  flex-col">
@@ -192,17 +180,16 @@ function App() {
             </>
           )}
         </div>
-        {Object.keys(data).map((item, index) => {
+        {Object.keys(data).map((item) => {
           const testimonialData = data[item];
           return (
             <motion.div
-              key={index}
-              id={item}
-              className={`bg-white h-32 w-32 z-[100] rounded-[1000px] absolute flex justify-center items-center shadow-[rgba(0,0,0,0.24)_0px_3px_8px] cursor-pointer ${dynamicStyles[index]}`}
+              key={item}
+              className={`bg-white p-2 h-16 w-16 md:h-20 md:w-20 lg:h-26 lg:w-26 xl:w-32 xl:h-32  z-[100] rounded-[1000px] absolute flex justify-center items-center shadow-[rgba(0,0,0,0.24)_0px_3px_8px] hover:shadow-[rgba(0,0,0,0.35)_0px_5px_15px] cursor-pointer ${dynamicStyles[item]}`}
               style={{ x, y }}
               whileHover={{ scale: 1.05 }}
               onMouseEnter={() => setState(testimonialData)}
-              onMouseLeave={() => setState(val)}
+              onMouseLeave={() => setState(defaultContent)}
             >
               <img
                 alt="company-img"
--- src/utils/data.js
+++ src/utils/data.js
@@ -1,83 +1,83 @@
-export const data= {
-    a:{
-        id:0,
-        companyLogo:"/billgates.png",
-        userImg:"/user1.png",
-        userName:"abc",
-        companyName:"Bill and Melinda gates",
-        role:"developer",
-        testimonial:"t is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accide"
-    },
-    b:{
-        id:1,
-        companyLogo:"/giz.png",
-        userImg:"/user2.png",
-        userName:"def",
-        companyName:"GIZ",
-        role:"developer",
-        testimonial:"t is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accide"
-    },
-    c:{
-        id:2,
-        companyLogo:"/google.png",
-        userImg:"/user1.png",
-        userName:"ghi",
-        companyName:"Google",
-        role:"developer",
-        testimonial:"t is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accide"
-    },
-    d:{
-        id:3,
-        companyLogo:"/iitb.png",
-        userImg:"/user2.png",
-        userName:"xyz",
-        companyName:"IITB",
-        role:"developer",
-        testimonial:"t is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accide"
-    },
-    e:{
-        id:4,
-        companyLogo:"/iitm.png",
-        userImg:"/user1.png",
-        userName:"NIL",
-        companyName:"IITM",
-        role:"developer",
-        testimonial:"t is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accide"
-    },
-    f:{
-        id:5,
-        companyLogo:"/microsoft.png",
-        userImg:"/user2.png",
-        userName:"NAN",
-        companyName:"Microsoft",
-        role:"developer",
-        testimonial:"t is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accide"
-    },
-    g:{
-        id:6,
-        companyLogo:"/mit.png",
-        userImg:"/user1.png",
-        userName:"John",
-        companyName:"MIT",
-        role:"developer",
-        testimonial:"t is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accide"
-    },
-    h:{
-        id:7,
-        companyLogo:"/ola.png",
-        userImg:"/user2.png",
-        userName:"HSH",
-        companyName:"Ola",
-        role:"developer",
-        testimonial:"t is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accide"
-    },
-    i:{
-        id:8,
-        companyLogo:"/wadhwani.png",
-        userImg:"/user1.png",
-        userName:"uwuw",
-        companyName:"Wadhwani AI",
-        role:"developer",
-        testimonial:"t is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accide"
-    }
-}
+export const data = {
+  0: {
+    companyLogo: "/billgates.png",
+    userImg: "/user1.png",
+    userName: "abc",
+    companyName: "Bill and Melinda gates",
+    role: "developer",
+    testimonial:
+      "t is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accide",
+  },
+  1: {
+    companyLogo: "/giz.png",
+    userImg: "/user2.png",
+    userName: "def",
+    companyName: "GIZ",
+    role: "developer",
+    testimonial:
+      "t is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accide",
+  },
+  2: {
+    companyLogo: "/google.png",
+    userImg: "/user1.png",
+    userName: "ghi",
+    companyName: "Google",
+    role: "developer",
+    testimonial:
+      "t is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accide",
+  },
+  3: {
+    companyLogo: "/iitb.png",
+    userImg: "/user2.png",
+    userName: "xyz",
+    companyName: "IITB",
+    role: "developer",
+    testimonial:
+      "t is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accide",
+  },
+  4: {
+    companyLogo: "/iitm.png",
+    userImg: "/user1.png",
+    userName: "NIL",
+    companyName: "IITM",
+    role: "developer",
+    testimonial:
+      "t is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accide",
+  },
+  5: {
+    companyLogo: "/microsoft.png",
+    userImg: "/user2.png",
+    userName: "NAN",
+    companyName: "Microsoft",
+    role: "developer",
+    testimonial:
+      "t is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accide",
+  },
+  6: {
+    companyLogo: "/mit.png",
+    userImg: "/user1.png",
+    userName: "John",
+    companyName: "MIT",
+    role: "developer",
+    testimonial:
+      "t is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accide",
+  },
+  7: {
+    companyLogo: "/ola.png",
+    userImg: "/user2.png",
+    userName: "HSH",
+    companyName: "Ola",
+    role: "developer",
+    testimonial:
+      "t is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accide",
+  },
+  8: {
+    companyLogo: "/wadhwani.png",
+    userImg: "/user1.png",
+    userName: "uwuw",
+    companyName: "Wadhwani AI",
+    role: "developer",
+    testimonial:
+      "t is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accide",
+  },
+};
