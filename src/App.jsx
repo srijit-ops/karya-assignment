364ae9343f253eb679c508f7dc9c69c063fb0ad3
srijit-ops <srijitasengupta23@gmail.com>
2024-06-25 14:41:37 +0530

    fixes applied

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,13 @@
-import { useMotionValue, motion } from "framer-motion";
+import { useMotionValue, motion, AnimatePresence } from "framer-motion";
 import { useRef, useState } from "react";
 import { data } from "./utils/data";
 
-const moveRestrictionVertical = 25;
-const moveRestrictionHorizontal = 25;
+const moveRestriction = 17;
+const moveRestriction0 = 40;
+const moveRestriction1 = 18;
+const moveRestriction2 = 15;
+const moveRestriction3 = 11;
+const moveRestriction4 = 8;
 
 const defaultContent = {
   mainLogo: "/karya-logo.svg",
@@ -22,8 +26,20 @@ const dynamicStyles = [
 ];
 
 function App() {
-  const x = useMotionValue(0);
-  const y = useMotionValue(0);
+  const motionValues = {
+    x: useMotionValue(0),
+    y: useMotionValue(0),
+    x0: useMotionValue(0),
+    y0: useMotionValue(0),
+    x1: useMotionValue(0),
+    y1: useMotionValue(0),
+    x2: useMotionValue(0),
+    y2: useMotionValue(0),
+    x3: useMotionValue(0),
+    y3: useMotionValue(0),
+    x4: useMotionValue(0),
+    y4: useMotionValue(0),
+  };
 
   const ref = useRef(null);
 
@@ -39,17 +55,71 @@ function App() {
         (e.clientY - rect.top) / rect.height;
 
       const objectX = Math.floor(
-        -moveRestrictionHorizontal / 2 +
-          moveRestrictionHorizontal * mousePositionHorizontalFromMiddle
+        -moveRestriction / 2 +
+          moveRestriction * mousePositionHorizontalFromMiddle
       );
 
       const objectY = Math.floor(
-        -moveRestrictionVertical / 2 +
-          moveRestrictionVertical * mousePositionVerticalFromMiddle
+        -moveRestriction / 2 + moveRestriction * mousePositionVerticalFromMiddle
+      );
+      const objectX0 = Math.floor(
+        -moveRestriction0 / 2 +
+          moveRestriction0 * mousePositionHorizontalFromMiddle
       );
 
-      x.set(objectX);
-      y.set(objectY);
+      const objectY0 = Math.floor(
+        -moveRestriction0 / 2 +
+          moveRestriction0 * mousePositionVerticalFromMiddle
+      );
+      const objectX1 = Math.floor(
+        -moveRestriction1 / 2 +
+          moveRestriction1 * mousePositionHorizontalFromMiddle
+      );
+
+      const objectY1 = Math.floor(
+        -moveRestriction1 / 2 +
+          moveRestriction1 * mousePositionVerticalFromMiddle
+      );
+      const objectX2 = Math.floor(
+        -moveRestriction2 / 2 +
+          moveRestriction2 * mousePositionHorizontalFromMiddle
+      );
+
+      const objectY2 = Math.floor(
+        -moveRestriction2 / 2 +
+          moveRestriction2 * mousePositionVerticalFromMiddle
+      );
+      const objectX3 = Math.floor(
+        -moveRestriction3 / 2 +
+          moveRestriction3 * mousePositionHorizontalFromMiddle
+      );
+
+      const objectY3 = Math.floor(
+        -moveRestriction3 / 2 +
+          moveRestriction3 * mousePositionVerticalFromMiddle
+      );
+      const objectX4 = Math.floor(
+        -moveRestriction4 / 2 +
+          moveRestriction4 * mousePositionHorizontalFromMiddle
+      );
+
+      const objectY4 = Math.floor(
+        -moveRestriction4 / 2 +
+          moveRestriction4 * mousePositionVerticalFromMiddle
+      );
+
+      motionValues.x.set(objectX);
+      motionValues.y.set(objectY);
+      motionValues.x0.set(objectX0);
+      motionValues.y0.set(objectY0);
+      motionValues.x1.set(objectX1);
+      motionValues.y1.set(objectY1);
+      motionValues.x2.set(objectX2);
+      motionValues.y2.set(objectY2);
+      motionValues.x3.set(objectX3);
+      motionValues.y3.set(objectY3);
+      motionValues.x4.set(objectX4);
+      motionValues.y4.set(objectY4);
     }
   };
 
@@ -76,18 +146,7 @@ function App() {
             stroke="#bfe9d3"
             strokeWidth="2"
             strokeDasharray="6 10"
-            style={{ x, y }}
-          ></motion.circle>
-          <motion.circle
-            cx="700"
-            cy="700"
-            r="658.5"
-            fill="#d7f8e8"
-            fillOpacity="0.2"
-            stroke="#bfe9d3"
-            strokeWidth="2"
-            strokeDasharray="4 10"
-            style={{ x, y }}
+            style={{ x: motionValues.x4, y: motionValues.y4 }}
           ></motion.circle>
           <motion.circle
             cx="700"
@@ -98,7 +157,7 @@ function App() {
             stroke="#bfe9d3"
             strokeWidth="2"
             strokeDasharray="9 10"
-            style={{ x, y }}
+            style={{ x: motionValues.x3, y: motionValues.y3 }}
           ></motion.circle>
           <motion.circle
             cx="700"
@@ -109,7 +168,7 @@ function App() {
             stroke="#b7e7ce"
             strokeWidth="2"
             strokeDasharray="6 10"
-            style={{ x, y }}
+            style={{ x: motionValues.x2, y: motionValues.y2 }}
           ></motion.circle>
           <motion.circle
             cx="700"
@@ -120,18 +179,7 @@ function App() {
             stroke="#b4e6cb"
             strokeWidth="2"
             strokeDasharray="4 10"
-            style={{ x, y }}
-          ></motion.circle>
-          <motion.circle
-            cx="700"
-            cy="700"
-            r="398.5"
-            fill="#d7f8e8"
-            fillOpacity="0.2"
-            stroke="#b8deca"
-            strokeWidth="2"
-            strokeDasharray="6 10"
-            style={{ x, y }}
+            style={{ x: motionValues.x1, y: motionValues.y1 }}
           ></motion.circle>
           <motion.circle
             cx="700"
@@ -142,43 +190,57 @@ function App() {
             stroke="#8cdcb2"
             strokeWidth="2"
             strokeDasharray="4 10"
-            style={{ x, y }}
+            style={{ x: motionValues.x0, y: motionValues.y0 }}
           ></motion.circle>
         </svg>
         <div className="absolute top-1/2 left-1/2 z-[500] h-[350px] sm:w-[420px] w-11/12 pointer-events-none -translate-x-1/2 -translate-y-1/2 select-none flex justify-center items-center flex-col">
-          {state.mainLogo ? (
-            <div className="flex justify-center items-center">
-              <img
-                alt="karya-logo"
-                src={state.mainLogo}
-                className="lg:w-96 lg:h-96 sm:w-72 sm:h-72 w-56 h-56"
-              />
-            </div>
-          ) : (
-            <>
-              <h1 className="font-bold md:text-xl sm:text-lg text-base tracking-wide sm:mb-8 mb-6 text-[#1e293b] text-center">
-                Trusted by the world's top companies
-              </h1>
-              <div className="flex justify-center  flex-col">
-                <p className="sm:text-base text-sm">{state.testimonial}</p>
-                <div className="flex justify-start items-start gap-4 mt-8">
-                  <img
-                    alt="user-img"
-                    src={state.userImg}
-                    className="w-70 h-70"
-                  />
-                  <div>
-                    <p className="text-[#41b47d] sm:text-lg text-base font-bold">
-                      {state.userName}
-                    </p>
-                    <p className="sm:text-base text-sm">
-                      {state.role}, {state.companyName}
-                    </p>
+          <AnimatePresence>
+            {state.mainLogo && (
+              <motion.div
+                className="flex justify-center items-center absolute"
+                animate={{ opacity: 1 }}
+                initial={{ opacity: 0 }}
+                exit={{ opacity: 0 }}
+              >
+                <img
+                  alt="karya-logo"
+                  src={state.mainLogo}
+                  className="lg:w-96 lg:h-96 sm:w-72 sm:h-72 w-56 h-56"
+                />
+              </motion.div>
+            )}
+          </AnimatePresence>
+          <AnimatePresence>
+            {!state.mainLogo && (
+              <motion.div
+                animate={{ opacity: 1 }}
+                initial={{ opacity: 0 }}
+                exit={{ opacity: 0 }}
+              >
+                <h1 className="font-bold md:text-xl sm:text-lg text-base tracking-wide sm:mb-8 mb-6 text-[#1e293b] text-center">
+                  Trusted by the world's top companies
+                </h1>
+                <div className="flex justify-center  flex-col">
+                  <p className="sm:text-base text-sm">{state.testimonial}</p>
+                  <div className="flex justify-start items-start gap-4 mt-8">
+                    <img
+                      alt="user-img"
+                      src={state.userImg}
+                      className="w-70 h-70"
+                    />
+                    <div>
+                      <p className="text-[#41b47d] sm:text-lg text-base font-bold">
+                        {state.userName}
+                      </p>
+                      <p className="sm:text-base text-sm">
+                        {state.role}, {state.companyName}
+                      </p>
+                    </div>
                   </div>
                 </div>
-              </div>
-            </>
-          )}
+              </motion.div>
+            )}
+          </AnimatePresence>
         </div>
         {Object.keys(data).map((item) => {
           const testimonialData = data[item];
@@ -186,7 +248,7 @@ function App() {
             <motion.div
               key={item}
               className={`bg-white p-2 h-16 w-16 md:h-20 md:w-20 lg:h-26 lg:w-26 xl:w-32 xl:h-32  z-[100] rounded-[1000px] absolute flex justify-center items-center shadow-[rgba(0,0,0,0.24)_0px_3px_8px] hover:shadow-[rgba(0,0,0,0.35)_0px_5px_15px] cursor-pointer ${dynamicStyles[item]}`}
-              style={{ x, y }}
+              style={{ x: motionValues.x, y: motionValues.y }}
               whileHover={{ scale: 1.05 }}
               onMouseEnter={() => setState(testimonialData)}
               onMouseLeave={() => setState(defaultContent)}
